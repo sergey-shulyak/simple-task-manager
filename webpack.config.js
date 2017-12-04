16935452e67e665d4b3b7e158f68e3c3d19cd696
Sergey Shulyak <sergey.shulyak@nixsolutions.com>
2017-12-04 14:46:29 +0200

    NJS-1413 Added Base app structure & configured webpack

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+
+const BUILD_DIR = path.resolve(__dirname, 'public/build')
+const SRC_DIR = path.resolve(__dirname, 'src')
+
+const config = {
+    entry: `${SRC_DIR}/index.js`,
+    output: {
+        path: BUILD_DIR,
+        filename: 'app.js'
+    },
+    module: {
+        rules: [
+            { test: /\.js$/, include: SRC_DIR, use: 'babel-loader' }
+        ]
+    },
+    devServer: {
+        contentBase: [path.join(__dirname, "public")],
+        inline: true
+    }
+}
+
+module.exports = config
