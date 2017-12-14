7d769d9bf16b0c723d21172233d2e62216b30a7c
Sergey Shulyak <sergey.shulyak@nixsolutions.com>
2017-12-14 17:02:57 +0200

    Fixed oldState overwrite

--- src/store/reducers/ui.js
+++ src/store/reducers/ui.js
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux';
 import { handleActions } from 'redux-actions';
+import omit from 'lodash/omit';
+
 import { setBoardsError, setTicketsError } from '../actions/ui';
 
 const defaultState = {};
 
 const errors = handleActions({
-  [setBoardsError]: (state, { payload }) => ({ boards: payload.message, ...state }),
-  [setTicketsError]: (state, { payload }) => ({ tickets: payload.message, ...state })
+  [setBoardsError]: (state, { payload }) => ({ boards: payload.message, ...omit(state, 'boards') }),
+  [setTicketsError]: (state, { payload }) => ({ tickets: payload.message, ...omit(state, 'tickets') })
 }, defaultState);
 
 export default combineReducers({
