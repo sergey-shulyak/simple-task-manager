44cdfe5160822f02a603f3a604ef35660737b2c0
Sergey Shulyak <shulyak@shulyak-lx.nixsolutions.com>
2018-01-15 15:16:29 +0200

    Refactored server endpoints

--- server/rest/tickets.js
+++ server/rest/tickets.js
@@ -1,45 +1,26 @@
 const express = require('express');
 const api = require('../persistence/tickets');
+const { passErrorNext } = require('./utils');
 
 const router = express.Router({ mergeParams: true });
 
 router.route('/tickets')
-  .get(async (req, res, next) => {
-    try {
-      res.json(await api.getTickets(req.params.boardId));
-    } catch (error) {
-      next(error);
-    }
-  })
-  .post(async (req, res, next) => {
-    try {
-      res.json(await api.createTicket(req.params.boardId, req.body));
-    } catch (error) {
-      next(error);
-    }
-  });
+  .get(passErrorNext(async (req, res) =>
+    res.json(await api.getTickets(req.params.boardId))
+  ))
+  .post(passErrorNext(async (req, res) =>
+    res.json(await api.createTicket(req.params.boardId, req.body))
+  ));
 
 router.route('/tickets/:ticketId')
-  .get(async (req, res, next) => {
-    try {
-      res.json(await api.getTicket(req.params.ticketId));
-    } catch (error) {
-      next(error);
-    }
-  })
-  .put(async (req, res, next) => {
-    try {
-      res.json(await api.updateTicket(req.body));
-    } catch (error) {
-      next(error);
-    }
-  })
-  .delete(async (req, res, next) => {
-    try {
-      res.json(await api.deleteTicket(req.params.ticketId));
-    } catch (error) {
-      next(error);
-    }
-  });
+  .get(passErrorNext(async (req, res) =>
+    res.json(await api.getTicket(req.params.ticketId))
+  ))
+  .put(passErrorNext(async (req, res) =>
+    res.json(await api.updateTicket(req.body))
+  ))
+  .delete(passErrorNext(async (req, res) =>
+    res.json(await api.deleteTicket(req.params.ticketId))
+  ));
 
 module.exports = router;
