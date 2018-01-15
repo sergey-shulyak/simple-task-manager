b19cb37948825d98d0b965349d16eab9c9fc5b41
Sergey Shulyak <shulyak@shulyak-lx.nixsolutions.com>
2018-01-15 17:45:04 +0200

    More server refactoring

--- server/persistence/boards.js
+++ server/persistence/boards.js
@@ -1,89 +1,70 @@
 const { ObjectId } = require('mongodb');
-const { getCollection, renameId, populateColumnsWithIdOrDefault } = require('./utils');
+const {
+  rejectOnError,
+  getCollection,
+  renameId,
+  populateColumnsWithIdOrDefault
+} = require('./utils');
 
-const getBoardsCollection = () => new Promise(async (resolve, reject) => {
-  try {
-    const boardCollection = await getCollection('boards');
-    resolve(boardCollection);
-  } catch (error) {
-    reject(error);
-  }
-});
+const getBoardsCollection = () => new Promise(rejectOnError(async (resolve) => {
+  const boardCollection = await getCollection('boards');
+  resolve(boardCollection);
+}));
 
-const getBoards = () => new Promise(async (resolve, reject) => {
-  try {
-    const boardCollection = await getBoardsCollection();
-    const boards = await boardCollection.find().toArray();
+const getBoards = () => new Promise(rejectOnError(async (resolve) => {
+  const boardCollection = await getBoardsCollection();
+  const boards = await boardCollection.find().toArray();
 
-    resolve(renameId(...boards));
-  } catch (error) {
-    reject(error);
-  }
-});
+  resolve(renameId(...boards));
+}));
 
-const getBoard = id => new Promise(async (resolve, reject) => {
-  try {
-    const boardCollection = await getBoardsCollection();
-    const _id = new ObjectId(id);
+const getBoard = id => new Promise(rejectOnError(async (resolve) => {
+  const boardCollection = await getBoardsCollection();
+  const _id = new ObjectId(id);
 
-    const board = await boardCollection.findOne({ _id });
+  const board = await boardCollection.findOne({ _id });
 
-    resolve(...renameId(board));
-  } catch (error) {
-    reject(error);
-  }
-});
+  resolve(...renameId(board));
+}));
 
-const createBoard = board => new Promise(async (resolve, reject) => {
-  try {
-    const boardCollection = await getBoardsCollection();
+const createBoard = board => new Promise(rejectOnError(async (resolve) => {
+  const boardCollection = await getBoardsCollection();
 
-    const { columns, ...props } = board;
-    const columnsWithIds = populateColumnsWithIdOrDefault(columns);
+  const { columns, ...props } = board;
+  const columnsWithIds = populateColumnsWithIdOrDefault(columns);
 
-    const { ops } = await boardCollection.insertOne({
-      ...props, columns: columnsWithIds
-    });
+  const { ops } = await boardCollection.insertOne({
+    ...props, columns: columnsWithIds
+  });
 
-    const createdBoard = ops[0];
+  const createdBoard = ops[0];
 
-    resolve(createdBoard);
-  } catch (error) {
-    reject(error);
-  }
-});
+  resolve(createdBoard);
+}));
 
-const updateBoard = board => new Promise(async (resolve, reject) => {
-  try {
-    const { id, columns, ...rest } = board;
-    const _id = new ObjectId(id);
+const updateBoard = board => new Promise(rejectOnError(async (resolve) => {
+  const { id, columns, ...rest } = board;
+  const _id = new ObjectId(id);
 
-    const existingColumns = (await getBoard(id)).columns;
-    const columnsWithId = populateColumnsWithIdOrDefault(columns, existingColumns);
+  const existingColumns = (await getBoard(id)).columns;
+  const columnsWithId = populateColumnsWithIdOrDefault(columns, existingColumns);
 
-    const boardCollection = await getBoardsCollection();
-    const { modifiedCount } = await boardCollection.updateOne({ _id }, {
-      $set: { ...rest, columns: columnsWithId }
-    });
+  const boardCollection = await getBoardsCollection();
+  const { modifiedCount } = await boardCollection.updateOne({ _id }, {
+    $set: { ...rest, columns: columnsWithId }
+  });
 
-    resolve({ id, modified: modifiedCount > 0 });
-  } catch (error) {
-    reject(error);
-  }
-});
+  resolve({ id, modified: modifiedCount > 0 });
+}));
 
-const deleteBoard = id => new Promise(async (resolve, reject) => {
-  try {
-    const boardCollection = await getBoardsCollection();
-    const _id = new ObjectId(id);
+const deleteBoard = id => new Promise(rejectOnError(async (resolve) => {
+  const boardCollection = await getBoardsCollection();
+  const _id = new ObjectId(id);
 
-    const { deletedCount } = await boardCollection.deleteOne({ _id });
+  const { deletedCount } = await boardCollection.deleteOne({ _id });
 
-    resolve({ id, deleted: deletedCount > 0 });
-  } catch (error) {
-    reject(error);
-  }
-});
+  resolve({ id, deleted: deletedCount > 0 });
+}));
 
 module.exports = {
   getBoardsCollection,
